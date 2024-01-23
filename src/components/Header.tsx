3762abd2f1c29d878e5d80066dfbf7fa69a22c9b
Aisamuddin <aisam0094@gmail.com>
2024-01-23 22:07:07 +0800

    feat: update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,61 @@
+import { useAuth } from "@/authContext";
 import { GraduationCap } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface HeaderProps {
-  username: string;
-  role: string;
+  username?: string;
+  role?: string;
 }
 
 function Header({ username, role }: Readonly<HeaderProps>) {
+  const auth = useAuth();
+
+  function onSubmit() {
+    auth.logout?.();
+  }
+
   return (
     <header className="flex justify-between items-center border-solid border-b-2 border-y-gray-400 w-full py-5">
       <div className="flex items-center">
         <GraduationCap className="ml-2 h-8 w-8" />
         <h1 className="ml-3 font-bold text-3xl">DOSAS</h1>
       </div>
-      <div className="flex">
+      <div className="flex items-center">
         <div className="flex px-3">
           <div>Hello {username}</div>
           <span className="ml-3 text-gray-500">({role})</span>
         </div>
-        <div className="px-3 cursor-pointer">Logout</div>
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <button className="text-md no-underline font-bold mx-2">
+              Log Out
+            </button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                You will be logging out of this session.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={() => onSubmit()}>
+                Log Out
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </header>
   );
